6d3af3def9630dab78bfd81aec5bb17bc1388cad
Mateus Silva <83253336+Mateus046@users.noreply.github.com>
2022-12-01 20:48:42 -0300

    adicionado novas depencencias

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-const express = require ('express');
-const conta = express();
-
-console.log('test of github');
-
-conta.post('/cadastroConta', (req, res) => {
-    res.send('Rota para cadastro de conta');
-});
-
-conta.get('/listarConta', (req, res) => {
-    res.send('Rota para  listar conta');
-});
-
-conta.put('/alterarConta', (req, res) => {
-    res.send('Rota para  alterar conta');
-});
-
-conta.delete('/excluirConta', (req, res) => {
-    res.send('Rota para excluir conta');
-});
+const express = require('express');
+const routesCategoria = require('./route/routesCategorias');
 
 
 const app = express();
-app.listen(3000,()=>{
-    console.log('server online!');
 
-});
+app.use(express.json());
 
-app.get('/', function (req,res){
-    res.send('Teste de aplicação!!!')
+console.log('teste de github')
+
+app.use('/', routesCategoria);
+
+
+app.listen(3000, ()=>{
+    console.log('SERVIDOR RODANDO EM - http://localhost:3000');
 });
